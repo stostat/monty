be652f5fbf8d8969cbd01cd9b1a56d2ede53d837
stostat <santmendieta@icloud.com>
2020-09-17 21:08:57 -0500

    test

---
+++ file_reader.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+many vari = {NULL, NULL, NULL};
+
+/**
+ * main - main function
+ * @ac: count of arguments
+ * @av: vectors
+ * Return: void
+ */
+int main(int ac, char *av[])
+{
+	char *line = NULL;
+	size_t len = 0;
+	int i = 1;
+	char delimit[] = " \t\r\v\n$";
+	stack_t *stack = NULL;
+
+	if (ac != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	if (ismonty(av[1]) == 0)
+	{
+		dprintf(2, "Error: Can't open file %s %s", av[1], "\n");
+		exit(EXIT_FAILURE);
+	}
+	vari.monty_file = fopen(av[1], "r");
+	if (!vari.monty_file)
+	{
+		dprintf(2, "Error: Can't open file %s %s", av[1], "\n");
+		exit(EXIT_FAILURE);
+	}
+	while ((getline(&line, &len, vari.monty_file)) != -1)
+	{
+		vari.token = _strtok(line, delimit);
+		check_monty(vari.token, i);
+		i++;
+	}
+	free(line);
+	free_close(&stack);
+	exit(EXIT_SUCCESS);
+}
---
+++ monty.h
@@ -0,0 +1,70 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * struct many_var - struct to header and FILE
+ * @monty_file: file to be opened
+ * @top: head of the stack
+ * @token: tokenized token
+  */
+typedef struct many_var
+{
+	FILE *monty_file;
+	stack_t *top;
+	char **token;
+} many;
+extern many vari;
+
+int ismonty(char *file);
+int _strlen_recursion(char *s);
+int _strcmp(char *s1, char *s2);
+char **_strtok(char *str, char *delimiter);
+int count_words(char *str, char *delimiter);
+int count_letters(char *str, char *delimiter);
+int _delimiters(char c, char *delimiter);
+int check_monty(char **token, unsigned int numline);
+void free_tokens(char **tokens);
+int _atoi(char *s);
+
+void free_close(stack_t **stack);
+void _push(stack_t **stack, unsigned int numline);
+void _nop(stack_t **stack, unsigned int numline);
+void _add(stack_t **stack, unsigned int numline);
+void _swap(stack_t **stack, unsigned int numline);
+void _pop(stack_t **stack, unsigned int numline);
+void _pint(stack_t **stack, unsigned int numline);
+void _pall(stack_t **stack, unsigned int numline);
+
+#endif
