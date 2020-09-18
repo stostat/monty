285399941dbe02247848c70bda508123a10b5a2b
stostat <santmendieta@icloud.com>
2020-09-17 21:05:51 -0500

    string manipulation files

---
+++ strchecker.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+
+/**
+ * ismonty - checks if a file has .m extension
+ * @file: str to be checked
+ * Return: 0 if no match found, 1 if succesful
+ */
+int ismonty(char *file)
+{
+	size_t len = _strlen_recursion(file) - 2;
+
+	if (len < 2)
+		return (0);
+	return (_strcmp(file + len, ".m") == 0);
+}
+
+/**
+ * _strlen_recursion - check the lenght of a string.
+ * @s: char
+ * Return: void.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * _strcmp - check the code for Holberton School students.
+ * @s1: char1
+ * @s2: char 2
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+	{
+		if (*(s1 + i) > *(s2 + i))
+			return (*(s1 + i) - *(s2 + i));
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: string to be reviewed
+ *
+ * Return: an integer from s or 0 if is not a number
+ */
+
+int _atoi(char *s)
+{
+	unsigned int runner, minus, num, letter, retrieved;
+
+	runner = num = letter = retrieved = 0;
+	minus = 1;
+
+	while (s[runner])
+	{
+		if (s[runner] == 45)
+			minus *= -1;
+		if (s[runner] >= 48 && s[runner] <= 57)
+		{
+			retrieved = retrieved * 10 + (s[runner] - 48);
+			num = 1;
+		}
+		if (!(s[runner] >= 48 && s[runner] <= 57))
+		{
+			if (num == 1)
+				letter = 1;
+		}
+		if (letter == 1)
+			break;
+		runner++;
+	}
+	return (retrieved * minus);
+}
---
+++ tokens.c
@@ -0,0 +1,121 @@
+#include "monty.h"
+
+/**
+ * _strtok - splits a string into words
+ * @str: given string
+ * @delimiter: array of strings that containt the specific delimiter
+ * Return: a pointer to an array of strings (words)
+ * NULL if str == NULL or str == ""
+ */
+char **_strtok(char *str, char *delimiter)
+{
+	char *strs = NULL, **new = NULL;
+	int index = 0, words, w, letters, l;
+
+	 strs = str;
+
+	if (strs == NULL || str[0] == 0)
+		return (NULL);
+
+	words = count_words(strs, delimiter);
+	if (!words)
+		return (NULL);
+
+	new = malloc(sizeof(char *) * (words + 1));
+	if (new == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (_delimiters(strs[index], delimiter))
+			index++;
+
+		letters = count_letters((strs + index), delimiter);
+
+		new[w] = malloc((letters + 1) * sizeof(char));
+		if (new[w] == NULL)
+		{
+			while (w--)
+				free(new[w]);
+			free(new);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			new[w][l] = strs[index++];
+
+		new[w][l] = 0;
+	}
+	new[w] = NULL;
+	return (new);
+}
+/**
+ * count_words - Counts the number of words in a given string
+ * @str: given string to count words
+ * @delimiter: string containing characters that indicate limits
+ * Return: number of words in str.
+ */
+int count_words(char *str, char *delimiter)
+{
+	int index, words = 0, length = 0;
+
+	for (index = 0; str[index]; index++)
+		length++;
+
+	for (index = 0; index < length; index++)
+	{
+		if (!_delimiters(str[index], delimiter) &&
+			(_delimiters(str[index + 1], delimiter) || !str[index + 1]))
+		{
+			words++;
+			index += count_letters(str + index, delimiter);
+		}
+	}
+
+	return (words);
+}
+/**
+ * count_letters - counts the number of letters of a word
+ * @str: string.
+ * @delimiter: string containing characters that indicate limits
+ * Return: number of letters of a word
+ */
+int count_letters(char *str, char *delimiter)
+{
+	int index = 0, letters = 0;
+
+	while (str[index] && !_delimiters(str[index], delimiter))
+	{
+		letters++;
+		index++;
+	}
+
+	return (letters);
+}
+/**
+ * _delimiters - finds characters in the command line to break args
+ *
+ * @c: character to be compared
+ * @delimiter: array of characters to be compared
+ * Return: on succes 1, else 0
+ */
+int _delimiters(char c, char *delimiter)
+{
+	for (; *delimiter;)
+	{
+		if (*delimiter++ == c)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * free_tokens - frees tokens
+ * @tokens: pointer to first element in tokens
+ * return: void
+ */
+void free_tokens(char **tokens)
+{
+	free(tokens);
+	tokens = NULL;
+}
